37d9bb82f339407a0779d362c05a1bc922d7a839
KareemHisham <kareem.hisham.ahmed@gmail.com>
2019-10-06 07:33:21 +0200

    Updates

---
+++ precache-manifest.ef6c3cf7cf659658c79abe0213c8e5d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc7681cd4b71466e7f40477aa9ddc13",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "946208e0ca5f0bc9cfb7",
+    "url": "/Weather-App/static/css/main.e518fc6a.chunk.css"
+  },
+  {
+    "revision": "62f5ad9ae91fa43db81e",
+    "url": "/Weather-App/static/js/2.ff2aed19.chunk.js"
+  },
+  {
+    "revision": "946208e0ca5f0bc9cfb7",
+    "url": "/Weather-App/static/js/main.47f9541b.chunk.js"
+  },
+  {
+    "revision": "c60b6d15c0ec5e08ae96",
+    "url": "/Weather-App/static/js/runtime-main.b0af46b4.js"
+  }
+]);
